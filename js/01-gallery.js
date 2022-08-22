4ee391a21ab72c28534a159e0fabc29ecae201ee
Roman <romanuch06.88@gmail.com>
2022-08-22 19:03:20 +0300

    Update 01-gallery.js
    need to be added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const gallerList = document.querySelector(".gallery");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+gallerList.insertAdjacentHTML("afterbegin", cardsMarkup);
+gallerList.addEventListener("click", onPictureClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.containes("gallery__item")) {
+    return;
+  }
+}
